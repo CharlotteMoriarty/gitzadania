9da50c6a9d52140b13418b4a7106bd72dc097258
CharlotteMoriarty <31165670+CharlotteMoriarty@users.noreply.github.com>
2017-10-22 15:58:40 +0200

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,63 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-    grunt.initConfig({    
+    grunt.initConfig({
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'main.css': 'main.sass'
+                }
+            }
+        },
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'images/build/'
+                }]
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['sass/*.sass'],
+                tasks: ['sass'],
+                options: {
+                spawn: false,
+                },
+            }
+        },
+        //browserSync
+        browserSync: {
+            default_options: {
+                bsFiles: {
+                    src: [
+                        "css/*.css",
+                        "*.html"
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: "./"
+                    }
+                }
+            }
+        }
     });
-
-    grunt.registerTask('work', function(){   
+//test
+    grunt.registerTask('work', function () {
         console.log('It does not work');
-    });    
+    });
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-watch');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.registerTask('default', ['browserSync', 'watch']);
+    grunt.registerTask('default', ['sass', 'imagemin','watch']);
+    
 };
